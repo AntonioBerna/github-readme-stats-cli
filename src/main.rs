f28d6e39636c35c37b36a5672c6f3d148e3258d3
AntonioBerna <asukeasukai12@gmail.com>
2023-11-17 19:55:43 +0100

    command line arguments became optional :technologist:

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,24 @@ use reqwest;
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
     let args: Vec<String> = env::args().collect();
-    if args.len() != 4 {
-        eprintln!("Usage: {} <username> <langs_count> <hide>", args[0]);
+    if args.len() < 2 || args.len() > 4 {
+        eprintln!("Usage: {} <username> <optional=[langs_count], [hide]>", args[0]);
         std::process::exit(1);
     }
 
     let username: &String = args.get(1).expect("Username not provided");
-    let langs_count: usize = args.get(2).and_then(|s: &String| s.parse().ok()).expect("Invalid langs_count");
-    let hide: &String = args.get(3).expect("Hide not provided");
-    let url: String = format!("https://github-readme-stats.vercel.app/api/top-langs/?username={username}&langs_count={langs_count}&hide={hide}");
+    let mut langs_count: usize = 5;
+    let mut hide: &str = "";
+    
+    if args.len() > 2 {
+        langs_count = args.get(2).and_then(|s: &String| s.parse().ok()).unwrap_or(5);
+    }
 
+    if args.len() > 3 {
+        hide = args.get(3).map(|s: &String| s.as_str()).unwrap_or("");
+    }
+
+    let url: String = format!("https://github-readme-stats.vercel.app/api/top-langs/?username={username}&langs_count={langs_count}&hide={hide}");
     let body: String = reqwest::get(&url).await?.text().await?;
     let document: Document = Document::from(body.as_str());
     let mut index: i32 = 1;
