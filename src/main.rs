af7cb651d20d886482cbc325c04b073b27296d85
AntonioBerna <asukeasukai12@gmail.com>
2024-08-06 03:34:31 +0200

    added explicit types :fire:

--- src/main.rs
+++ src/main.rs
@@ -9,25 +9,23 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         std::process::exit(1);
     }
 
-    let username = &args[1];
-    let langs_count = args.get(2).and_then(|s| s.parse().ok()).unwrap_or(5);
-    let hide = args.get(3).map_or("", |s| s.as_str());
+    let username: &String = &args[1];
+    let langs_count: i32 = args.get(2).and_then(|s| s.parse().ok()).unwrap_or(5);
+    let hide: &str = args.get(3).map_or("", |s| s.as_str());
 
-    let url = format!("https://github-readme-stats.vercel.app/api/top-langs/?username={}&langs_count={}&hide={}", username, langs_count, hide);
-    let body = ureq::get(&url).call()?.into_string()?;
+    let url: String = format!("https://github-readme-stats.vercel.app/api/top-langs/?username={}&langs_count={}&hide={}", username, langs_count, hide);
+    let body: String = ureq::get(&url).call()?.into_string()?;
     
-    // Use regex to find the language names
-    let re = Regex::new(r#"<text.*?>(.*?)</text>"#)?;
-    let mut index = 1;
+    let re: Regex = Regex::new(r#"<text.*?>(.*?)</text>"#)?;
+    let mut index: i32 = 1;
 
     println!("{} Most Used Languages", username);
-
     for cap in re.captures_iter(&body) {
-        let formatted_name = &cap[1];
+        let formatted_name: &str = &cap[1];
         if index % 2 != 0 {
             print!("{:2} - {:11}", (index + 1) / 2, formatted_name);
         } else {
-            print!("[ {:7}]\n", formatted_name);
+            println!("[ {:7}]", formatted_name);
         }
         io::stdout().flush()?;
         index += 1;
